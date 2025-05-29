b2a83aba75e27e4981962c126980a2c8a4850b6e
Emirhan Koç <19010208038@ogrenci.bartin.edu.tr>
2025-05-29 23:22:51 +0300

    Fix: Socket connection for HTTPS

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ const io = new Server(server, {
 });
 
 const PORT = process.env.PORT || 5000;
-const users = {}; // { username: socket.id }
-const socketToUser = {}; // { socket.id: username }
+const users = {};
 
 app.use(cors());
 app.use(express.json());
@@ -29,62 +28,46 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('🔌 Bağlandı:', socket.id);
 
-  // Kullanıcı kaydı
-  socket.on('register', (username) => {
+  socket.on("register", (username) => {
     users[username] = socket.id;
-    socketToUser[socket.id] = username;
-    console.log(`${username} sisteme girdi.`);
-
-    io.emit('userList', Object.keys(users)); // Tüm kullanıcılara listeyi gönder
+    console.log(`${username} kayıt oldu (${socket.id})`);
   });
 
-  // Mesaj gönderme
   socket.on('sendMessage', (data) => {
-    const { from, to, message } = data;
-    const toSocket = users[to];
-    if (toSocket) {
-      io.to(toSocket).emit('receiveMessage', { from, message });
-      socket.emit('receiveMessage', { from, message }); // mesajı kendine de göster
-    }
+    io.emit('receiveMessage', data);
   });
 
-  // Yazıyor bildirimi
-  socket.on('typing', (to) => {
-    const toSocket = users[to];
-    if (toSocket) {
-      io.to(toSocket).emit('showTyping', socketToUser[socket.id]);
-    }
+  socket.on('typing', () => {
+    socket.broadcast.emit('showTyping');
   });
 
-  // 📞 Arama başlat
-  socket.on('callUser', (data) => {
-    const targetSocketId = users[data.toUsername];
-    if (targetSocketId) {
-      io.to(targetSocketId).emit('callIncoming', {
-        from: socket.id,
+  socket.on('startCall', (data) => {
+    const from = socket.id;
+    const targetSocket = Object.values(users).find(id => id !== from);
+    if (targetSocket) {
+      io.to(targetSocket).emit('incomingCall', {
+        from: from,
         name: data.name,
-        signal: data.signal,
+        signal: data.signal
       });
     }
   });
 
-  // ✅ Arama kabul edildi
   socket.on('answerCall', (data) => {
     io.to(data.to).emit('callAccepted', data.signal);
   });
 
-  // 🔁 Bağlantı koparsa
   socket.on('disconnect', () => {
-    const username = socketToUser[socket.id];
-    if (username) {
-      delete users[username];
-      delete socketToUser[socket.id];
-      console.log(`${username} çıkış yaptı`);
-      io.emit('userList', Object.keys(users));
+    for (let key in users) {
+      if (users[key] === socket.id) {
+        delete users[key];
+        console.log(`${key} çıkış yaptı`);
+        break;
+      }
     }
   });
 });
 
 server.listen(PORT, () => {
-  console.log(`🚀 Sunucu ayakta: http://localhost:${PORT}`);
+  console.log(`🚀 Sunucu ayakta: ${PORT}`);
 });
