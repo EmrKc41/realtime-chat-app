7ef8089e37d50afafcc19f67a01e51ef543b4ae1
Emirhan Koç <19010208038@ogrenci.bartin.edu.tr>
2025-05-29 23:03:37 +0300

    Fix: Socket connection for HTTPS

--- server.js
+++ server.js
@@ -16,86 +16,58 @@ const io = new Server(server, {
 });
 
 const PORT = process.env.PORT || 5000;
+const users = {};
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const users = {};       // username -> socket.id
-const sockets = {};     // socket.id -> username
-const chatPairs = {};   // socket.id -> targetSocket.id
-
 io.on('connection', (socket) => {
-  console.log(`🔌 Yeni bağlantı: ${socket.id}`);
+  console.log('🔌 Bağlandı:', socket.id);
 
-  // Kullanıcı kaydı
-  socket.on('register', (username) => {
+  socket.on("register", (username) => {
     users[username] = socket.id;
-    sockets[socket.id] = username;
-    console.log(`✅ Kayıt: ${username} -> ${socket.id}`);
-    io.emit('userList', Object.keys(users)); // sohbet listesini güncelle
+    console.log(`${username} kayıt oldu (${socket.id})`);
   });
 
-  // Sohbet partneri belirle
-  socket.on('setChatWith', (targetUsername) => {
-    const partnerId = users[targetUsername];
-    if (partnerId) {
-      chatPairs[socket.id] = partnerId;
-      console.log(`💬 ${sockets[socket.id]} artık ${targetUsername} ile sohbet ediyor.`);
-    }
+  socket.on('sendMessage', (data) => {
+    io.emit('receiveMessage', data);
   });
 
-  // Mesaj gönder
-  socket.on('sendMessage', ({ username, message }) => {
-    const partnerId = chatPairs[socket.id];
-    if (partnerId) {
-      io.to(partnerId).emit('receiveMessage', { username, message });
-      socket.emit('receiveMessage', { username, message }); // kendi ekranına da yansıt
-    }
-  });
-
-  // Yazıyor bildirimi
   socket.on('typing', () => {
-    const partnerId = chatPairs[socket.id];
-    if (partnerId) {
-      io.to(partnerId).emit('showTyping');
-    }
+    socket.broadcast.emit('showTyping');
   });
 
-  // Çağrı başlat
-  socket.on('callUser', ({ signal }) => {
-    const targetId = chatPairs[socket.id];
-    if (targetId) {
-      io.to(targetId).emit('callIncoming', {
-        from: socket.id,
-        name: sockets[socket.id],
-        signal
+  socket.on('startCall', (data) => {
+    const from = socket.id;
+    const targetSocket = Object.values(users).find(id => id !== from);
+    if (targetSocket) {
+      io.to(targetSocket).emit('incomingCall', {
+        from: from,
+        name: data.name,
+        signal: data.signal
       });
     }
   });
 
-  // Çağrı kabul edildi
-  socket.on('answerCall', ({ to, signal }) => {
-    io.to(to).emit('callAccepted', signal);
+  socket.on('answerCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
   });
 
-  // Bağlantı koparsa
   socket.on('disconnect', () => {
-    const username = sockets[socket.id];
-    if (username) {
-      delete users[username];
+    for (let key in users) {
+      if (users[key] === socket.id) {
+        delete users[key];
+        console.log(`${key} çıkış yaptı`);
+        break;
+      }
     }
-    delete sockets[socket.id];
-    delete chatPairs[socket.id];
-    io.emit('userList', Object.keys(users));
-    console.log(`❌ ${username || socket.id} bağlantısı koptu`);
   });
 });
 
 server.listen(PORT, () => {
-  console.log(`🚀 Sunucu ayakta: http://localhost:${PORT}`);
+  console.log(`🚀 Sunucu ayakta: ${PORT}`);
 });
